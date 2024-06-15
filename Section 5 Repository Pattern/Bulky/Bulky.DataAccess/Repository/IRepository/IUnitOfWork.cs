bb91ea31f54564e6bc1fccc56655db1e2615e410
Bikash Dumrakoti <bikasdumrakoti@gmail.com>
2024-06-15 15:40:00 +0545

    Completed Section 5: Repository Pattern. Implemented repository pattern, unit of work, areas, and dropdown in navigation item in the existing project.

---
+++ Section 5 Repository Pattern/Bulky/Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        void Save();
+    }
+}
---
+++ Section 5 Repository Pattern/Bulky/Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
