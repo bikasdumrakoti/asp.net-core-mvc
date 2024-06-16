e6fcc638216f946dfa46370a97c786e590e4ad8a
Bikash Dumrakoti <bikasdumrakoti@gmail.com>
2024-06-16 22:20:29 +0545

    Completed Section 6: Product CRUD.

---
+++ Section 6 Product CRUD/Bulky/Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
+        void Save();
+    }
+}
