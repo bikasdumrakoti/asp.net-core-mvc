c5a1d526ed1e0b5cd4a4cab3d142f80d2810f2b2
Bikash Dumrakoti <bikasdumrakoti@gmail.com>
2024-06-18 16:39:42 +0545

    Completed Section 9: Company CRUD.

---
+++ Section 9 Company CRUD/Bulky/Bulky.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            var objFromDb = _db.Companies.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.StreetAddress = obj.StreetAddress;
+                objFromDb.City = obj.City;
+                objFromDb.State = obj.State;
+                objFromDb.PostalCode = obj.PostalCode;
+                objFromDb.PhoneNumber = obj.PhoneNumber;
+            }
+        }
+    }
+}
---
+++ Section 9 Company CRUD/Bulky/Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
+        ICompanyRepository Company { get; }
+
+        void Save();
+    }
+}
