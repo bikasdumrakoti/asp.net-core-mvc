d818b618ee1a87e99c2ce110c6acae510a74f9b3
Bikash Dumrakoti <bikasdumrakoti@gmail.com>
2024-06-22 13:22:49 +0545

    Completed Section 15: User Management.

---
+++ Section 15 User Management/Bulky/Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System.Linq.Expressions;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ Section 15 User Management/Bulky/Bulky.Models/OrderDetail.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+        public double Price { get; set; }
+    }
+}
